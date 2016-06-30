bb4286f8d958a9e7dc94ab0ed7aef55f6a244b62
Joe Wass <jwass@crossref.org>
2016-06-30 12:07:17 +0100

    Install script.

--- etc/install.sh
+++ etc/install.sh
@@ -1,4 +1,11 @@
 # to be run from base directory
+set -e
+echo "Compile JAR"
 sudo -u deploy lein uberjar
+echo "Install service"
 cp etc/*.service /etc/systemd/system/
+echo "Reload services"
 systemctl daemon-reload
+echo "Restart service"
+service event-data-live-stream restart
+echo "Done!"
